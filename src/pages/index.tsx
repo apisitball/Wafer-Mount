0132dfc29a6c5bab30e21955dec13bb866340949
apisitball <apisit16112538@gmail.com>
2024-07-05 11:11:01 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 
 const WaferMount = () => {
   const [diex, setDiex] = useState<any>(0);
@@ -7,31 +9,27 @@ const WaferMount = () => {
   const [sumdie, setSumDie] = useState<number | string>(0);
   const [sumarea, setSumArea] = useState<number | string>(0);
   const [error, setError] = useState<string>();
+  const router = useRouter();
 
-  function calculate() {
-    setSumDie((diex * diey) / 10);
+  useEffect(() => {
+    setSumDie((diey * diex) / 10);
     setSumArea(((dimeter / 2) * 22 * (dimeter / 2)) / 7);
-
-    if (sumdie >= sumarea) {
+    if ((diey * diex) / 10 > ((dimeter / 2) * 22 * (dimeter / 2)) / 7) {
       setError("PASS");
     } else {
       setError("FAIL");
     }
-  }
-  function handleReset() {
-    setDiex(""),
-      setDiey(""),
-      setDimeter(""),
-      setSumDie(""),
-      setSumDie(""),
-      setSumArea(""),
-      setError("");
-  }
+    setTimeout(() => {
+      router.reload();
+    }, 300000);
+    
+  }, [diex, diey, dimeter]);
+
   return (
     <div className="header">
       <h1>Calculator 10% Die Size</h1>
-
       <div>
+      
         <div className="inpuda">
           <h2>
             Die size X
@@ -39,7 +37,7 @@ const WaferMount = () => {
           </h2>
           <input
             className="inputBox"
-            placeholder="ใส่ค่า Die X"
+            placeholder=" Die X"
             type="number"
             value={diex}
             onChange={(e) => setDiex(+e.target.value)}
@@ -53,7 +51,7 @@ const WaferMount = () => {
           </h2>
           <input
             className="inputBox"
-            placeholder="ใส่ค่า Die y"
+            placeholder=" Die y"
             type="number"
             value={diey}
             onChange={(e) => setDiey(+e.target.value)}
@@ -64,27 +62,35 @@ const WaferMount = () => {
           <h2>Bubble Dimeter :&nbsp;&nbsp;</h2>
           <input
             className="inputBox"
-            placeholder="ใส่ค่า Dimeter"
+            placeholder="Dimeter"
             type="number"
             value={dimeter}
             onChange={(e) => setDimeter(+e.target.value)}
           />
           <h3>&nbsp;&nbsp;ml</h3>
-          </div>
-          <div className="sum">10% Die Size : &nbsp;&nbsp;{sumdie || ""}&nbsp;&nbsp;sqmm</div>
-          <div className="sum">Bubble Area :&nbsp;&nbsp;{sumarea || ""}&nbsp;&nbsp;sqmm</div>
-        <div>
-          <button className="btnCal" onMouseMove={calculate}>
-            คำนวน
-          </button>
-          <button className="btnClear" onClick={handleReset}>
-            Reset{" "}
+        </div>
+        <div className="sum">
+          10% Die Size : &nbsp;&nbsp;{sumdie || ""}&nbsp;&nbsp;sqmm
+        </div>
+        <div className="sum">
+          Bubble Area :&nbsp;&nbsp;{sumarea || ""}&nbsp;&nbsp;sqmm
+        </div><br/>
+        <div >
+          <button className="button-73"
+            type="button"
+            onClick={() => {
+              router.reload();
+              alert ("Confirm Reset");
+            }}
+          >
+            Click here to Reset
           </button>
         </div>
+        
         <div>
           <h1 className={error === "PASS" ? "fontGreen" : "fontRed"}>
             {error}
-          </h1>
+          </h1>      
         </div>
       </div>
     </div>
